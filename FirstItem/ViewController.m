526bd9d20b750c673924d2b6bf889c6131ebfef7
FJW-GitHub <xiaofu@xiaofudeMacBook-Pro.local>
2016-03-04 10:07:16 +0800

    init

--- FirstItem/ViewController.m
+++ FirstItem/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //1.0已加载完成
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
